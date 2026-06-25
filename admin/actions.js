47e1b69d66c0d5ced1de7419deed5dafb0734560
s3r <9calebofficial@gmail.com>
2026-06-25 08:49:06 +0800

    Update actions.js

--- admin/actions.js
+++ admin/actions.js
@@ -1,1 +1,11 @@
+showConfirm({
+    title:"Approve Request?",
+    text:"This request will appear on Screen.",
+    confirmText:"Approve",
+    type:"success",
+    onConfirm:async()=>{
 
+        await approveRequest(id);
+
+    }
+});
